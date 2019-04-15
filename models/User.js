fd0e4a800e9b010acd7a3cdec7df9d7a9d011d36
joann675 <joann675@yahoo.com>
2019-04-15 18:17:16 -0400

    initial commit of server code

---
+++ models/User.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new NoteSchema object
+// This is similar to a Sequelize model
+var UserSchema = new Schema({
+  // `title` is of type String
+  userid: String,
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 }
+  
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var User = mongoose.model("User", UserSchema);
+
+// Export the Note model
+module.exports = User;
